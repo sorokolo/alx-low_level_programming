1dba4920bd3a176a39a9186954563d44eb876f7c
sorokolo <ksoro17@alustudent.com>
2020-09-16 22:29:01 +0200

    -

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
+#include <limits.h>
 
 /**
-*main - Printing types
-*
-*Return: 0 at the end of the function
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("Size of a char: %ld byte(s)\n", sizeof(char));
@@ -13,4 +14,4 @@ int main(void)
 	printf("Size of a long long int: %ld byte(s)\n", sizeof(long long int));
 	printf("Size of a float: %ld byte(s)\n", sizeof(float));
 	return (0);
-{
+}
