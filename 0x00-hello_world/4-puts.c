d9aeb117ff9e1fd1ff3ed3e3eb78135682ec0baa
sorokolo <ksoro17@alustudent.com>
2020-09-16 21:10:19 +0200

    -

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+*main - Printing text
+*
+*Return: 0 at the end of the function
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
