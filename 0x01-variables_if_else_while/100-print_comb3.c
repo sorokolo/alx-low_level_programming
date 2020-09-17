99218b34d8c553d689de62a90ace6302e5719f87
sorokolo <ksoro17@alustudent.com>
2020-09-17 21:56:16 +0200

    -

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,34 +1,46 @@
+#include <stdlib.h>
+#include <time.h>
 #include <stdio.h>
 
+/* betty style doc for function main goes there */
 /**
- * main - prints a list of double didgits
+ * main - Entry point
  *
- * Description: digits should not be repeated twice
- * Return: Always(0) Success
-*/
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	int j;
 
-int main (void){
-    int i, j;
-    
-    for (i = 48; i < 58; i++){
-        for (j = 48; j < 58; j++){
-            if (i == j){
-                continue;
-            }
+	i = 48;
+	j = 49;
+	while  ((i < 57) && (j < 58))
+	{
+		putchar(i);
+		putchar(j);
+		if ((i == 56) && (j == 57))
+		{
+			putchar('\n');
+			i++;
+			j++;
+		}
+		else
+		{
+			putchar(44);
+			putchar(32);
+			if (j < 57)
+			{
+				j++;
+			}
+			else
+			{
+				i++;
+				j = 1 + i;
+			}
+		}
 
-            putchar(i);
-            putchar(j);
+	}
 
-            if (i == 56 && j == 57){
-                break;
-
-            } else {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-
-    putchar('\n');
-    return (0);
+	return (0);
 }
