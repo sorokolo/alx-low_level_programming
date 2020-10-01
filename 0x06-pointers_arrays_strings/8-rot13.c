23ecda1c7809bbb7632f70d7f5134a3728356379
sorokolo <ksoro17@alustudent.com>
2020-10-01 21:12:43 +0200

    -

--- 0x06-pointers_arrays_strings/8-rot13.c
+++ 0x06-pointers_arrays_strings/8-rot13.c
@@ -1,46 +1,47 @@
 #include "holberton.h"
 
 /**
- * rot13 - Encodes a string using rot13.
- * @str: The string to be encoded.
- *
- * Description : 
- * Return: A pointer to the encoded string.
- */
+* rot13 - encodes a string using rot13.
+* @str: the string to be encoded.
+*
+* Return: A pointer to the encoded string.
+*/
+
 char *rot13(char *str)
 {
-    int indx1 = 0, indx2;
-    char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
-                         'G', 'H', 'I', 'J', 'K', 'L',
-                         'M', 'N', 'O', 'P', 'Q', 'R',
-                         'S', 'T', 'U', 'V', 'W', 'X',
-                         'Y', 'Z', 'a', 'b', 'c', 'd',
-                         'e', 'f', 'g', 'h', 'i', 'j',
-                         'k', 'l', 'm', 'n', 'o', 'p',
-                         'q', 'r', 's', 't', 'u', 'v',
-                         'w', 'x', 'y', 'z'};
-    char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
-                         'T', 'U', 'V', 'W', 'X', 'Y',
-                         'Z', 'A', 'B', 'C', 'D', 'E',
-                         'F', 'G', 'H', 'I', 'J', 'K',
-                         'L', 'M', 'n', 'o', 'p', 'q',
-                         'r', 's', 't', 'u', 'v', 'w',
-                         'x', 'y', 'z', 'a', 'b', 'c',
-                         'd', 'e', 'f', 'g', 'h', 'i',
-                         'j', 'k', 'l', 'm'};
+	int i = 0;
+	int j;
+	char alphabet[52] = {'A', 'B', 'C', 'D', 'E', 'F',
+			     'G', 'H', 'I', 'J', 'K', 'L',
+			     'M', 'N', 'O', 'P', 'Q', 'R',
+			     'S', 'T', 'U', 'V', 'W', 'X',
+			     'Y', 'Z', 'a', 'b', 'c', 'd',
+			     'e', 'f', 'g', 'h', 'i', 'j',
+			     'k', 'l', 'm', 'n', 'o', 'p',
+			     'q', 'r', 's', 't', 'u', 'v',
+			     'w', 'x', 'y', 'z'};
+	char rot13key[52] = {'N', 'O', 'P', 'Q', 'R', 'S',
+			     'T', 'U', 'V', 'W', 'X', 'Y',
+			     'Z', 'A', 'B', 'C', 'D', 'E',
+			     'F', 'G', 'H', 'I', 'J', 'K',
+			     'L', 'M', 'n', 'o', 'p', 'q',
+			     'r', 's', 't', 'u', 'v', 'w',
+			     'x', 'y', 'z', 'a', 'b', 'c',
+			     'd', 'e', 'f', 'g', 'h', 'i',
+			     'j', 'k', 'l', 'm'};
+	while (str[i])
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == alphabet[j])
+			{
+				str[i] = rot13key[j];
+				break;
+			}
+		}
 
-    while (str[indx1])
-    {
-        for (indx2 = 0; indx2 < 52; indx2++)
-        {
-            if (str[indx1] == alphabet[indx2])
-            {
-                str[indx1] = rot13key[indx2];
-                break;
-            }
-        }
+		i++;
+	}
 
-        indx1++;
-    }
-    return (str);
+	return (str);
 }
