711b9298b038822d70c11f1fe05c5284c5ac94ed
sorokolo <ksoro17@alustudent.com>
2020-10-01 20:17:16 +0200

    -

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,19 +9,16 @@
 char *_strcat(char *dest, char *src)
 {
     char *appended = dest;
-
     while (*dest != '\0')
     {
         dest++;
     }
-
     while (*src != '\0')
     {
         *dest = *src;
         dest++;
         src++;
     }
-
     *dest = '\0';
     return (appended);
 }
